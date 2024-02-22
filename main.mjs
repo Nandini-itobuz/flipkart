31748eb8305ce5e9fb85dfb85acb133b73776947
itobuz <itobuz@itobuzs-MacBook-Air.local>
2024-02-22 17:59:11 +0530

    wip

--- main.mjs
+++ main.mjs
@@ -18,10 +18,12 @@ const cartItems = get
   ? parsedGet.find((ele) => ele[currentMail])[currentMail]
   : [];
 const searchBar = document.getElementById("search-bar");
-let users = JSON.parse(localStorage.getItem("userCarts"));
-let findindexmail = users.find((ele) => Object.keys(ele)[0] === currentMail);
 const total = document.getElementById("cart-num");
 const nav = document.getElementsByClassName("nav");
+const inpText = document.getElementById("text-input");
+let users = JSON.parse(localStorage.getItem("userCarts"));
+let findindexmail = users.find((ele) => Object.keys(ele)[0] === currentMail);
+let arr = [];
 
 function setList(key, data) {
   if (typeof localStorage !== "undefined") {
@@ -158,92 +160,95 @@ function minusCartBtn(addtocartbtn, cartAddbtn, cartMinusbtn, i) {
   totalitems();
 }
 
+function main(items) {
+  for (let i = 0; i < items.length; i++) {
+    const cardGroupItem = document.createElement("div");
+    cardGroup.appendChild(cardGroupItem);
+    cardGroupItem.style.cssText = `width: 90%;
+                                        background-color :#107BD4;
+                                        text-align:center;
+                                        margin-bottom:10%`;
+    cardGroupItem.setAttribute("id", "cardGroupItem".concat(i));
+
+    const cardImg = document.createElement("img");
+    cardImg.setAttribute("src", items[i]["image"]);
+    cardGroupItem.appendChild(cardImg);
+    cardImg.style.cssText = `width: 100%; margin-bottom:5%`;
+
+    const cardText = document.createElement("div");
+    cardGroupItem.appendChild(cardText);
+
+    const cardName = document.createElement("h2");
+    cardText.appendChild(cardName);
+    cardName.textContent = items[i]["productName"];
+    cardName.style.cssText = `font-size:1.2rem;
+                                font-family: sans-serif;
+                                font-weight:600;
+                                padding: 0% 5%;
+                                color:white`;
+
+    const cardDetails = document.createElement("p");
+    cardText.appendChild(cardDetails);
+    cardDetails.textContent = items[i]["description"];
+    cardDetails.style.cssText = `margin-top :2%;
+                                    // text-align:start;
+                                    font-family: sans-serif;
+                                    font-size:0.8rem;
+                                    padding: 0% 10%;
+                                    color:white;`;
+
+    const cardPrice = document.createElement("h3");
+    cardText.appendChild(cardPrice);
+    cardPrice.textContent = "\u20b9".concat(items[i]["price"]);
+    cardPrice.style.cssText = `font-weight: 600;
+                                margin-top :3%;
+                                font-family: sans-serif;
+                                color:white;`;
+
+    const cardDiv = document.createElement("div");
+    cardText.appendChild(cardDiv);
+    cardDiv.style.cssText = `display:flex;justify-content:center `;
+
+    const cardMinusButton = document.createElement("button");
+    cardDiv.appendChild(cardMinusButton);
+    cardMinusButton.textContent = "-";
+    cardMinusButton.style.cssText = `margin:3% 1%;
+                                    padding:3%;
+                                    font-weight:600;
+                                    border:1px solid white;
+                                    color:#107BD4;
+                                    display:none;
+                                    border-radius:2%;`;
+    cardMinusButton.setAttribute("class", "cart-minusbtn".concat(i));
+
+    const cardButton = document.createElement("button");
+    cardDiv.appendChild(cardButton);
+    cardButton.textContent = "Add to Cart";
+    cardButton.style.cssText = `margin:3%;
+                                    padding:3%;
+                                    font-weight:600;
+                                    border:1px solid white;
+                                    color:#107BD4;
+                                    border-radius:2%;`;
+    cardButton.setAttribute("class", "cart-btn".concat(i));
+
+    const cardAddButton = document.createElement("button");
+    cardDiv.appendChild(cardAddButton);
+    cardAddButton.textContent = "+";
+    cardAddButton.style.cssText = `margin:3% 1%;
+                                    padding:3%;
+                                    font-weight:600;
+                                    border:1px solid white;
+                                    color:#107BD4;
+                                    display:none;
+                                    border-radius:2%;`;
+    cardAddButton.setAttribute("class", "cart-addbtn".concat(i));
+  }
+}
+
 productPagetheme();
 totalitems();
-
-for (let i = 0; i < 8; i++) {
-  const cardGroupItem = document.createElement("div");
-  cardGroup.appendChild(cardGroupItem);
-  cardGroupItem.style.cssText = `width: 90%;
-                                      background-color :#107BD4;
-                                      text-align:center;
-                                      margin-bottom:10%`;
-  cardGroupItem.setAttribute("id", "cardGroupItem".concat(i));
-
-  const cardImg = document.createElement("img");
-  cardImg.setAttribute("src", items[i]["image"]);
-  cardGroupItem.appendChild(cardImg);
-  cardImg.style.cssText = `width: 100%; margin-bottom:5%`;
-
-  const cardText = document.createElement("div");
-  cardGroupItem.appendChild(cardText);
-
-  const cardName = document.createElement("h2");
-  cardText.appendChild(cardName);
-  cardName.textContent = items[i]["productName"];
-  cardName.style.cssText = `font-size:1.2rem;
-                              font-family: sans-serif;
-                              font-weight:600;
-                              padding: 0% 5%;
-                              color:white`;
-
-  const cardDetails = document.createElement("p");
-  cardText.appendChild(cardDetails);
-  cardDetails.textContent = items[i]["description"];
-  cardDetails.style.cssText = `margin-top :2%;
-                                  // text-align:start;
-                                  font-family: sans-serif;
-                                  font-size:0.8rem;
-                                  padding: 0% 10%;
-                                  color:white;`;
-
-  const cardPrice = document.createElement("h3");
-  cardText.appendChild(cardPrice);
-  cardPrice.textContent = "\u20b9".concat(items[i]["price"]);
-  cardPrice.style.cssText = `font-weight: 600;
-                              margin-top :3%;
-                              font-family: sans-serif;
-                              color:white;`;
-
-  const cardDiv = document.createElement("div");
-  cardText.appendChild(cardDiv);
-  cardDiv.style.cssText = `display:flex;justify-content:center `;
-
-  const cardMinusButton = document.createElement("button");
-  cardDiv.appendChild(cardMinusButton);
-  cardMinusButton.textContent = "-";
-  cardMinusButton.style.cssText = `margin:3% 1%;
-                                  padding:3%;
-                                  font-weight:600;
-                                  border:1px solid white;
-                                  color:#107BD4;
-                                  display:none;
-                                  border-radius:2%;`;
-  cardMinusButton.setAttribute("class", "cart-minusbtn".concat(i));
-
-  const cardButton = document.createElement("button");
-  cardDiv.appendChild(cardButton);
-  cardButton.textContent = "Add to Cart";
-  cardButton.style.cssText = `margin:3%;
-                                  padding:3%;
-                                  font-weight:600;
-                                  border:1px solid white;
-                                  color:#107BD4;
-                                  border-radius:2%;`;
-  cardButton.setAttribute("class", "cart-btn".concat(i));
-
-  const cardAddButton = document.createElement("button");
-  cardDiv.appendChild(cardAddButton);
-  cardAddButton.textContent = "+";
-  cardAddButton.style.cssText = `margin:3% 1%;
-                                  padding:3%;
-                                  font-weight:600;
-                                  border:1px solid white;
-                                  color:#107BD4;
-                                  display:none;
-                                  border-radius:2%;`;
-  cardAddButton.setAttribute("class", "cart-addbtn".concat(i));
-}
+main(items);
 
 cardGroup.addEventListener("click", function (event) {
   for (let i = 0; i < 8; i++) {
@@ -265,4 +270,14 @@ cardGroup.addEventListener("click", function (event) {
       minusCartBtn(addtocartbtn[0], cartAddbtn[0], cartMinusbtn[0], i);
     }
   }
+});
+
+inpText.addEventListener("input", () => {
+  let val = inpText.value;
+  cardGroup.innerHTML = "";
+  arr = [];
+  items.filter((item) => {
+    if (item["productName"].toLowerCase().includes(val)) arr.push(item);
+  });
+  if (arr.length) main(arr);
 });
--- registration.js
+++ registration.js
@@ -1,73 +1,67 @@
-// import userDetails from "./userDetails.json" assert { type: 'json' };
-function setList(key, data) {
-    if (typeof localStorage !== "undefined") {
-      if (!localStorage.getItem(key)) {
-        localStorage.setItem(key, JSON.stringify(data));
-      }
-    } else {
-      console.error("localStorage is not available in this environment.");
-    }
-}
-
-function updateListItems(key,data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
-let users=[];
-setList("userDetails",users);
-let userDetails = JSON.parse(localStorage.getItem("userDetails"));
-
-
-const otpInput =document.getElementsByClassName("login-verify");
+const otpInput = document.getElementsByClassName("login-verify");
 const getotpbtn = document.getElementsByClassName("login-btn");
 const otpBtn = document.getElementsByClassName("verify-btn");
+const email = document.getElementsByClassName("inputfeild");
 otpBtn[0].style.display = "none";
 getotpbtn[2].style.display = "none";
+let users = [];
+setList("userDetails", users);
+let userDetails = JSON.parse(localStorage.getItem("userDetails"));
 
-const email = document.getElementsByClassName("inputfeild");
-console.log(email[4])
+function setList(key, data) {
+  if (typeof localStorage !== "undefined") {
+    if (!localStorage.getItem(key)) {
+      localStorage.setItem(key, JSON.stringify(data));
+    }
+  } else {
+    console.error("localStorage is not available in this environment.");
+  }
+}
+
+function updateListItems(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
 
 function sendOtp() {
-    let otpValue = Math.floor(Math.random() * 100000);
-    let emailBody = `<h2>Your OTP is </h2>${otpValue}`;
-    Email.send({
-        SecureToken: "866b863a-baa3-4b0c-b1fc-e15a24518420",
-        To: email[1].value,
-        From: "nandini@itobuz.com",
-        Subject: "Verify Email!",
-        Body: emailBody,
-    }).then((message) => {
-        if (message === "OK") {
-            alert("OTP sent to your email " + email[1].value);
+  let otpValue = Math.floor(Math.random() * 100000);
+  let emailBody = `<h2>Your OTP is </h2>${otpValue}`;
+  Email.send({
+    SecureToken: "866b863a-baa3-4b0c-b1fc-e15a24518420",
+    To: email[1].value,
+    From: "nandini@itobuz.com",
+    Subject: "Verify Email!",
+    Body: emailBody,
+  }).then((message) => {
+    if (message === "OK") {
+      alert("OTP sent to your email " + email[1].value);
 
-            otpInput[0].style.display ="block";
-            email[2].style.display = "block";
-            otpBtn[0].style.display = "block";
-            getotpbtn[0].style.display = "none";
-            console.log(email[3].value);
-            otpBtn[0].addEventListener("click", () => {
-                if (email[4].value == otpValue) {
-                    alert("Email Address Verified");
-                    if(message === "OK"){
-                        getotpbtn[2].style.display = "block";
-                        getotpbtn[1].style.display = "none";
+      otpInput[0].style.display = "block";
+      email[2].style.display = "block";
+      otpBtn[0].style.display = "block";
+      getotpbtn[0].style.display = "none";
+      console.log(email[3].value);
+      otpBtn[0].addEventListener("click", () => {
+        if (email[4].value == otpValue) {
+          alert("Email Address Verified");
+          if (message === "OK") {
+            getotpbtn[2].style.display = "block";
+            getotpbtn[1].style.display = "none";
 
-                        let userFormDetails = document.getElementsByClassName("inputfeild");
-                        const userObj = {
-                                        "userId": userDetails.length,
-                                        "name": userFormDetails[0].value,
-                                        "email" : userFormDetails[1].value,
-                                        "password" : userFormDetails[2].value,
-                                    }
-                        
-                        userDetails.push(userObj);
-                        updateListItems("userDetails",userDetails);
+            let userFormDetails = document.getElementsByClassName("inputfeild");
+            const userObj = {
+              userId: userDetails.length,
+              name: userFormDetails[0].value,
+              email: userFormDetails[1].value,
+              password: userFormDetails[2].value,
+            };
 
-                    }
-                } else {
-                    alert("invalid otp");
-                }
-            });
+            userDetails.push(userObj);
+            updateListItems("userDetails", userDetails);
+          }
+        } else {
+          alert("invalid otp");
         }
-    });
+      });
+    }
+  });
 }
